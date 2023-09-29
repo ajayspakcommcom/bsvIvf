29ba68bc8a217b58311971692f0f58a69913ac18
Ajay Mahavir Vishwakarma <ajayspak@spakcomm.com>
2023-09-29 15:22:39 +0530

    changes

--- controllers/config.js
+++ controllers/config.js
@@ -1,26 +1,8 @@
-// exports.dataBaseConfig = {
-//   server: "A2NWPLSK14SQL-v01.shr.prod.iad2.secureserver.net",
-//   user: "BSV_IVF",
-//   password: "4_uGdn74",
-//   port: 1433,
-//   database: "BSV_IVF",
-//   pool: {
-//     max: 10,
-//     min: 0,
-//     idleTimeoutMillis: 30000,
-//   },
-//   options: {
-//     encrypt: true, //for azure
-//     trustServerCertificate: true, //change to true for local dev / self-signed certs
-//   },
-// };
-
-
 exports.dataBaseConfig = {
-  server: "N1NWPLSK12SQL-v01.shr.prod.ams1.secureserver.net",
-  user: "hae",
-  password: "Spak@12345",
-  //port: 1433,
+  server: "A2NWPLSK14SQL-v01.shr.prod.iad2.secureserver.net",
+  user: "BSV_IVF",
+  password: "4_uGdn74",
+  port: 1433,
   database: "BSV_IVF",
   pool: {
     max: 10,
@@ -32,3 +14,21 @@ exports.dataBaseConfig = {
     trustServerCertificate: true, //change to true for local dev / self-signed certs
   },
 };
+
+
+// exports.dataBaseConfig = {
+//   server: "N1NWPLSK12SQL-v01.shr.prod.ams1.secureserver.net",
+//   user: "hae",
+//   password: "Spak@12345",
+//   //port: 1433,
+//   database: "BSV_IVF",
+//   pool: {
+//     max: 10,
+//     min: 0,
+//     idleTimeoutMillis: 30000,
+//   },
+//   options: {
+//     encrypt: true, //for azure
+//     trustServerCertificate: true, //change to true for local dev / self-signed certs
+//   },
+// };
