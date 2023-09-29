ad52e14574e5eb03986a4dbfb6d3840bb7fc024c
Ajay Mahavir Vishwakarma <ajayspak@spakcomm.com>
2023-09-29 15:08:50 +0530

    LATEST Changes

--- controllers/config.js
+++ controllers/config.js
@@ -1,7 +1,25 @@
+// exports.dataBaseConfig = {
+//   server: "A2NWPLSK14SQL-v01.shr.prod.iad2.secureserver.net",
+//   user: "BSV_IVF",
+//   password: "4_uGdn74",
+//   port: 1433,
+//   database: "BSV_IVF",
+//   pool: {
+//     max: 10,
+//     min: 0,
+//     idleTimeoutMillis: 30000,
+//   },
+//   options: {
+//     encrypt: true, //for azure
+//     trustServerCertificate: true, //change to true for local dev / self-signed certs
+//   },
+// };
+
+
 exports.dataBaseConfig = {
-  server: "A2NWPLSK14SQL-v01.shr.prod.iad2.secureserver.net",
-  user: "BSV_IVF",
-  password: "4_uGdn74",
+  server: "N1NWPLSK12SQL-v01.shr.prod.ams1.secureserver.net",
+  user: "hae",
+  password: "Spak@12345",
   port: 1433,
   database: "BSV_IVF",
   pool: {
